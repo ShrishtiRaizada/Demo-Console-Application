27c23dee1a0b2a1df77ef254b884b94cce36fdfb
Shrishti Raizada <70793477+ShrishtiRaizada@users.noreply.github.com>
2024-06-20 22:24:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Thread.Sleep(5000);
+        }
+    }
+}
